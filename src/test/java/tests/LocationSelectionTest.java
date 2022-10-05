1253ea9f846336261d472de9c6a47bb3790426f5
AliakseiPrystrom <pristrom1991@gmail.com>
2022-10-05 16:08:08 +0300

    preparing for CI/18

--- src/test/java/tests/LocationSelectionTest.java
+++ src/test/java/tests/LocationSelectionTest.java
@@ -1,10 +1,12 @@
 package tests;
 
 import drivers.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import utils.JsonDataUtil;
+
 
 public class LocationSelectionTest extends BaseTest {
 
@@ -13,6 +15,12 @@ public class LocationSelectionTest extends BaseTest {
 
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println(WebDriverManager.getDriver().manage().window().getSize());
+        Dimension dimension = new Dimension(1920, 1080);
+        WebDriverManager.getDriver().manage().window().setSize(dimension);
+        System.out.println(WebDriverManager.getDriver().manage().window().getSize());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+
 
         HomePage homePage = new HomePage()
                 .openPage()
