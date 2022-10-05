02437dd7a0f556f2f513905ccd2f9ee4c86590b3
AliakseiPrystrom <pristrom1991@gmail.com>
2022-10-05 14:36:51 +0300

    preparing for CI/14

--- src/test/java/tests/LocationSelectionTest.java
+++ src/test/java/tests/LocationSelectionTest.java
@@ -9,11 +9,11 @@ public class LocationSelectionTest extends BaseTest {
 
     @Test
     public void checkLocationSelection() {
-//        HomePage homePage = new HomePage()
-//                .openPage()
-//                .goToLocationSelection()
-//                .chooseLocation(JsonDataUtil.get("testdata", "location"))
-//                .goToShopping();
-//        Assert.assertTrue(homePage.checkCurrentLocation(), "current location fail");
+        HomePage homePage = new HomePage()
+                .openPage()
+                .goToLocationSelection()
+                .chooseLocation(JsonDataUtil.get("testdata", "location"))
+                .goToShopping();
+        Assert.assertTrue(homePage.checkCurrentLocation(), "current location fail");
     }
 }
